a38bb2d93da5abfb7841d7698f417d678846c305
Thomas Sam-Yin-Yang <thomas.samyinyang@gmail.com>
2021-05-27 22:24:31 -0400

    Created edit profile modal

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tours#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tours do
-    resources :events, only: [:new, :create]
+  resources :tours, except: [:new, :edit] do
+    resources :events, only: [:create]
   end
 
   resources :users, only: [:show, :edit, :update]
-  resources :events, only: [:show, :edit, :update, :destroy]
+  resources :events, only: [:show, :update, :destroy]
 end
