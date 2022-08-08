8da2594241584cc799653eedaba2fe941eae1741
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2022-08-08 09:47:29 -0400

    added gem fil e for flatpickr

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,5 +1,5 @@
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/themes/airbnb.css';
+import 'flatpickr/dist/themes/confetti.css';
 
 const flatPickrCal = () => {
   const fp = flatpickr("#tour_events_attributes_0_show_start_at", {
